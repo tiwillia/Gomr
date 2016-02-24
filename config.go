8d1ab42f574be20d199a8553a0e2c18e5d0682fb
tiwillia@redhat.com <tiwillia@redhat.com>
2016-02-23 20:24:28 -0500

    Enable openshift database usage

--- config.go
+++ config.go
@@ -3,6 +3,7 @@ package main
 import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"os"
 )
 
 // This struct defines the yaml the configuration file must follow
@@ -21,9 +22,45 @@ type Config struct {
 	}
 }
 
-// Set configuration variables from configuration file
-func GetConfiguration(filePath string) (c Config) {
-	confile, err := ioutil.ReadFile(filePath)
+var (
+	configFilePath = "./gomr.yaml"
+)
+
+// Set configuration variables from configuration file and environment
+func GetConfiguration() (c Config) {
+	// Bail if the configuration file isn't found
+	//  Some configuration must be set via configuration file
+	if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
+		panic("Configuration file " + configFilePath + " not found.")
+	}
+
+	// Get inital configuration from yaml file
+	c = getFileConfiguration()
+
+	// Set database configuration via env variables if they exist
+	//  Environment variables take precedence over file configuration
+	if e := os.Getenv("GOMR_DATABASE_SERVICE_HOST"); e != "" {
+		c.Db.Hostname = e
+	}
+	if e := os.Getenv("GOMR_DATABASE_SERVICE_PORT"); e != "" {
+		c.Db.Port = e
+	}
+	if e := os.Getenv("MYSQL_USER"); e != "" {
+		c.Db.Username = e
+	}
+	if e := os.Getenv("MYSQL_PASSWORD"); e != "" {
+		c.Db.Password = e
+	}
+	if e := os.Getenv("MYSQL_DATABASE"); e != "" {
+		c.Db.Name = e
+	}
+
+	return c
+}
+
+// Get configuration from yaml file
+func getFileConfiguration() (c Config) {
+	confile, err := ioutil.ReadFile(configFilePath)
 	if err != nil {
 		panic("Failed to read configuration: " + err.Error())
 	}
