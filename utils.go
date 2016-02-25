1d510d9716192fd2d5633c8e8c9375b81fe97642
tiwillia@redhat.com <tiwillia@redhat.com>
2016-02-25 14:20:13 -0500

    Added http get utility

--- utils.go
+++ utils.go
@@ -1,9 +1,42 @@
 package main
 
 import (
+	"errors"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"regexp"
+	"strconv"
 )
+
+// GET provided url over tcp. Returns a string with the response body.
+func HttpGet(url string) (response []byte, err error) {
+	client := &http.Client{}
+
+	var req *http.Request
+	req, err = http.NewRequest("GET", url, nil)
+	if err != nil {
+		return
+	}
+
+	var resp *http.Response
+	resp, err = client.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	response, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		return response, errors.New("HTTP status code: " + strconv.Itoa(resp.StatusCode))
+	}
+
+	return
+}
 
 // Test if 'match' matches 'line' and, if it does, return the substring indicated by regex 'pull'
 func MatchAndPull(line, regex, pull string) (substr string) {
